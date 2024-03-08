bca75d635f8aba18448e6e3bf8265187aa487f92
blackhat-7 <palshaunak7@gmail.com>
2024-03-08 16:39:55 +0530

    fix: lol added to readme

--- scripts/write_branch_commit.sh
+++ scripts/write_branch_commit.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Exit if the hook is supposed to be skipped
-if [ ! -z "$SKIP_POST_COMMIT_HOOK" ]; then
-    exit 0
-fi
-
 # Fetch current branch name
 branch_name=$(git branch --show-current)
 # Fetch latest commit hash
@@ -12,10 +7,6 @@ commit_hash=$(git rev-parse HEAD)
 
 # Define the file path
 file="pyproject.toml"
-
-# Read current branch and commit from pyproject.toml
-current_branch=$(grep "^branch =" $file | cut -d'"' -f2)
-current_commit=$(grep "^commit =" $file | cut -d'"' -f2)
 
 # Function to update pyproject.toml
 update_pyproject() {
@@ -33,21 +24,12 @@ update_pyproject() {
 updates_made=false
 
 # Check and update branch in pyproject.toml if necessary
-if [ "$branch_name" != "$current_branch" ]; then
-    update_pyproject "branch" "$branch_name"
-    updates_made=true
-fi
+update_pyproject "branch" "$branch_name"
 
 # Check and update commit in pyproject.toml if necessary
-if [ "$commit_hash" != "$current_commit" ]; then
-    update_pyproject "commit" "$commit_hash"
-    updates_made=true
-fi
+update_pyproject "commit" "$commit_hash"
 
 # Commit the changes only if updates were made and avoid the hook
-if [ "$updates_made" = true ]; then
-    git add $file
-    # Use a condition to prevent the hook from running or use an environment variable
-    export SKIP_POST_COMMIT_HOOK=1
-    git commit --amend --no-edit --no-verify
-fi
+git add $file
+# Use a condition to prevent the hook from running or use an environment variable
+git commit --amend --no-edit --no-verify
