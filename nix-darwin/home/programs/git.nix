59754077b8466ef6462110e77390c771875b078a
blackhat-7 <palshaunak7@gmail.com>
2026-03-15 00:44:16 +0530

    chutes gitleak

--- nix-darwin/home/programs/git.nix
+++ nix-darwin/home/programs/git.nix
@@ -15,7 +15,20 @@
     executable = true;
     text = ''
       #!/usr/bin/env bash
-      exec gitleaks protect --staged --verbose
+      exec gitleaks protect --staged --verbose --config "$HOME/.config/gitleaks/config.toml"
+    '';
+  };
+
+  home.file.".config/gitleaks/config.toml" = {
+    text = ''
+      [extend]
+      useDefault = true
+
+      [[rules]]
+      description = "Chutes API Key"
+      id = "chutes-api-key"
+      regex = "cpk_[0-9a-f]{32}\\.[0-9a-f]{32}\\.[0-9a-zA-Z]{32}"
+      tags = ["api", "chutes"]
     '';
   };
 }
