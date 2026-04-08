a1ba6d12f255ef0d72631e5a4213df0b71cfa966
blackhat-7 <palshaunak7@gmail.com>
2026-04-09 00:18:22 +0530

    chore: update aichat model paths to use simplified format

--- nix-darwin/home/programs/default.nix
+++ nix-darwin/home/programs/default.nix
@@ -23,7 +23,7 @@
     aichat = {
       enable = true;
       settings = {
-        model = "chutes/deepseek-ai/DeepSeek-V3.2-TEE";
+        model = "chutes:deepseek-ai/DeepSeek-V3.2";
         clients = [
           # {
           #   type = "openai-compatible";
@@ -44,7 +44,7 @@
             api_base = "https://llm.chutes.ai/v1";
             models = [
               {
-                name = "chutes/deepseek-ai/DeepSeek-V3.2-TEE";
+                name = "deepseek-ai/DeepSeek-V3.2";
                 supports_function_calling = true;
                 strip_reasoning_contents = true;
               }
