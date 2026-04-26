cc7e4983838fcb04c9222b37c7f3e82cbce6998b
blackhat-7 <palshaunak7@gmail.com>
2026-04-26 12:36:40 +0530

    feat: filter out scratchpad and popup‑w sessions from tmux‑jump selection.

--- scripts/tmux-jump.sh
+++ scripts/tmux-jump.sh
@@ -18,8 +18,10 @@ fi
 
 # Run fzf to pick a target window
 # Include pane current path and command so fzf can match on folder/command too
+# Exclude popup sessions (scratchpad, popup-w*) since they aren't switchable views.
 choice=$(
-    tmux list-windows -a -F '#{session_name}:#{window_index}	#{session_name}: #{window_name}  #{pane_current_path}  #{pane_current_command}' |
+    tmux list-windows -a -F '#{session_name}	#{session_name}:#{window_index}	#{session_name}: #{window_name}  #{pane_current_path}  #{pane_current_command}' |
+        awk -F'\t' '$1 != "scratchpad" && $1 !~ /^popup-w/ { print $2 "\t" $3 }' |
         fzf --prompt='jump> ' --reverse --height=100% --with-nth=2.. --delimiter='	' \
             --preview 'tmux capture-pane -ep -t {1}' --preview-window right:60%
 )
