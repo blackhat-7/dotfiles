54d49d1f8bf02b5be22d0e87cb2267de1529bee2
blackhat-7 <palshaunak7@gmail.com>
2026-06-06 01:44:56 +0530

    chore(nix-darwin): switch nixpkgs to nixos-unstable and add update/check Just recipes

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -2,7 +2,7 @@
   description = "Nix-Darwin Configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager";
