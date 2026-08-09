c6622554e48d1a46adb2412daa4f028ccc7a32da
blackhat-7 <palshaunak7@gmail.com>
2026-08-10 01:09:00 +0530

    fix(programs): disable fzf Ctrl-R history widget for atuin compatibility

--- nix-darwin/home/programs/default.nix
+++ nix-darwin/home/programs/default.nix
@@ -94,7 +94,11 @@ in
     lsd.enable = true;
     jq.enable = true;
     bat.enable = true;
-    fzf.enable = true;
+    fzf = {
+      enable = true;
+      # atuin owns Ctrl-R for history search
+      historyWidget.command = "";
+    };
     ripgrep.enable = true;
     # zed-editor.enable = true;
     vscode = {
