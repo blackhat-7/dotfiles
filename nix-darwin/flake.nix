572b23989cd7d25edf5cb7cacb2fe46a6ebada00
blackhat-7 <palshaunak7@gmail.com>
2026-09-17 15:16:54 +0530

    chore: update nixpkgs channel to nixpkgs-unstable and refresh lock

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -2,7 +2,7 @@
   description = "Nix-Darwin Configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     readonly-bash = {
